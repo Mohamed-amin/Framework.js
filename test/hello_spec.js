9580b96ee8d3c47eb6119aeff5d5987dc78b1eda
Amin <mohamed.amin.088@gmail.com>
2016-04-08 23:44:49 +0400

    Adding Lodash and jQuery

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -2,6 +2,6 @@ var sayHello = require('../src/hello.js');
 
 describe("hello", function(){
 	it("say hello", function(){
-		expect(sayHello()).toBe("Hello, World");
+		expect(sayHello('Amin')).toBe("Hello, Amin");
 	});
 });

e12dc1d099679373382eb612012ce7705c1edbd4
Amin <mohamed.amin.088@gmail.com>
2016-04-09 14:11:37 +0400

    fire the listner if the scope value change

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,26 @@
 'use strict';
+var _ = require('lodash');
 
 function Scope(){
-	
+	this.$$watchers = [];	
 }
+
+Scope.prototype.$watch = function(watcherFn, listnerFn) {
+	this.$$watchers.push({watcherFn:watcherFn, listnerFn:listnerFn});
+};
+
+Scope.prototype.$digest = function(){
+	var self = this;
+	var oldValue, newValue;
+	_.forEach(this.$$watchers, function(watcher){
+		newValue = watcher.watcherFn(self);
+		oldValue = watcher.last;
+		
+		if(oldValue !== newValue){
+			watcher.last = newValue;
+			watcher.listnerFn(newValue, oldValue, self);
+		}
+	});
+};
+
 module.exports = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,8 +2,52 @@ var Scope = require('../src/scope');
 
 describe("Scope",function(){
 	it('Can be contstructed and used as an object', function(){
-		var scope = new Scope;
+		var scope = new Scope();
 		scope.aProperty = 1;
 		expect(scope.aProperty).toBe(1);
+	});
+});
+describe('Digest',function(){
+	var scope;
+	beforeEach(function() {
+	    scope = new Scope();
+	});
+
+	it('Call the listner function of a watch on first $digest', function(){
+		var watcherFn = function(){return 'blah blah';};
+		var listnerFn = jasmine.createSpy();
+		scope.$watch(watcherFn, listnerFn);
+
+		scope.$digest();
+
+		expect(listnerFn).toHaveBeenCalled();
+	});
+
+	it('Calls the watcher funcion with scope as parameter', function(){
+		var watcherFn = jasmine.createSpy();
+		var listnerFn = function(){};
+		scope.$watch(watcherFn, listnerFn);
+
+		scope.$digest();
+
+		expect(watcherFn).toHaveBeenCalledWith(scope);
+	});
+
+	it('Calls the listner function when the scope value changes', function(){
+		scope.someValue = 'first';
+		scope.counter = 0;
+		scope.$watch(
+			function(scope){ return scope.someValue;},
+			function(newValue, oldValue, scope){ scope.counter++;}
+		);
+
+		expect(scope.counter).toBe(0);
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+		scope.$digest();
+		expect(scope.counter).toBe(1);
+		scope.someValue = 'Random updated value';
+		scope.$digest();
+		expect(scope.counter).toBe(2);
 	});
 });
